d9965739cd7aa40eb8d56bc093a95875a495ef9d
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-22 03:09:32 +0100

    the urls

---
+++ trader_project/trader_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .view_functions.user import *
+from django.contrib import admin
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('<int:trader_id>/', user_dashboard, name='user_dashboard'),
+    path('admin_dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('', home_to_create, name='home_to_create'),
+    path('admin_dashboard/<int:trader_id>/', trader_details, name='trader_details'),
+]
