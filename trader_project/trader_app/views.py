dec1d311af25a614f584dbdbbc267ce8c6151712
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-23 23:40:50 +0100

    modified to work with the djabgo version

--- trader_project/trader_app/views.py
+++ trader_project/trader_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from .models import Trader, Trade
-# Create your views here.
 import random
 from decimal import Decimal
 
 
 def simulate_profit_loss():
-    traders = Trader.objects.all()
+    traders = Trader.objects.filter(username__startswith='trader').all()
     for trader in traders:
-        profit_loss = Decimal(random.uniform(-10, 10))  # Convert float to Decimal
-        trader.balance += profit_loss
+        profit_loss = Decimal(random.uniform(-10, 10))
+        trader.balance = Decimal(trader.balance) + profit_loss
         trader.save()
 
         trade = Trade.objects.create(trader=trader, profit_loss=profit_loss)
         trade.save()
+
+        # print(f"Profit/Loss: {str(profit_loss)}")
