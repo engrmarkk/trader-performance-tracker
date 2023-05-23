e6eab5f036e6ef1ddd63b888e4d582005346c939
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-23 23:44:05 +0100

    users view function restriction and authorization

--- trader_project/trader_app/view_functions/user.py
+++ trader_project/trader_app/view_functions/user.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from ..models import Trade
 from ..models import Trader
 from ..views import simulate_profit_loss
+from django.contrib import messages
 import json
 import plotly.graph_objects as go
 from django.http import HttpResponseNotFound
 from ..create_trader import create_trader_func
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def home_to_create(request):
@@ -18,7 +20,13 @@ def home_to_create(request):
     return HttpResponse('<h1>Done simulating</h1>')
 
 
+@login_required(login_url='login')
 def user_dashboard(request, trader_id):
+    if not request.user.is_authenticated:
+        messages.error(request, 'You need to be logged-in')
+        return redirect('login')
+    if request.user.id != trader_id and not request.user.is_superuser:
+        return HttpResponseNotFound('<h1>Page not found</h1>')
     trades = Trade.objects.filter(trader=trader_id).order_by('timestamp')
     if not trades:
         return HttpResponseNotFound('<h1>Page not found</h1>')
@@ -43,14 +51,37 @@ def user_dashboard(request, trader_id):
                    })
 
 
+@login_required(login_url='login')
 def admin_dashboard(request):
-    traders = Trader.objects.all()
+    if not request.user.is_authenticated:
+        messages.error(request, 'You need to be logged-in')
+        return redirect('login')
+    if not request.user.is_superuser:
+        return HttpResponseNotFound('<h1>Admin access only</h1>')
+    traders = Trader.objects.filter(is_superuser=False).all
     return render(request, 'admin.html', {'traders': traders})
 
 
+@login_required(login_url='login')
 def trader_details(request, trader_id):
+    trader_profit_lost = {}
+    if not request.user.is_authenticated:
+        messages.error(request, 'You need to be logged-in')
+        return redirect('login')
+    if not request.user.is_superuser:
+        return HttpResponseNotFound('<h1>Admin access only</h1>')
     trade = Trade.objects.filter(trader=trader_id).all()
+    if not trade:
+        return HttpResponseNotFound('<h1>No details yet</h1>')
+    for t in trade:
+        prof = float(t.profit_loss)
+        trader_profit_lost[t.timestamp.isoformat()] = prof
+
+    # Reverse the dictionary
+    reversed_profit_lost = {value: key for key, value in trader_profit_lost.items()}
+
     return render(request, 'trader_details.html',
                   {'trades': trade[::-1],
                    'trader': trade[0].trader,
+                   'prof_lost': reversed_profit_lost,
                    })
